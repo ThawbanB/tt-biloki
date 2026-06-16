40ff9a069b5d24bd299e9bce04830ffdcf003624
Thawban Boujja <thawbanjabss@outlook.fr>
2026-06-16 11:32:14 +0200

    middleware admin

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,17 @@ Route::get('/', function () {
     return view('Auth');
 })->name('Auth');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-Route::delete('/delete-user/{id}', [AuthController::class, 'delete'])->name('delete-user');
 
-Route::get('/create-product', [ProductController::class, 'index'])->name('create-product');
-Route::post('/create-product', [ProductController::class, 'store'])->name('store-product');
-Route::delete('/delete-product/{id}', [ProductController::class, 'delete'])->name('delete-product');
-Route::get('/edit-product/{id}', [ProductController::class, 'edit'])->name('edit-product');
-Route::put('/update-product/{id}', [ProductController::class, 'update'])->name('update-product');
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::delete('/delete-user/{id}', [AuthController::class, 'delete'])->name('delete-user');
+    Route::get('/create-product', [ProductController::class, 'index'])->name('create-product');
+    Route::post('/create-product', [ProductController::class, 'store'])->name('store-product');
+    Route::delete('/delete-product/{id}', [ProductController::class, 'delete'])->name('delete-product');
+    Route::get('/edit-product/{id}', [ProductController::class, 'edit'])->name('edit-product');
+    Route::put('/update-product/{id}', [ProductController::class, 'update'])->name('update-product');
+});
